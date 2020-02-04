9f68531866e03ea1fbf57edddb465b75a6f32fb2
Yasmeen2080 <yasmeen2080@gmail.com>
2020-02-04 17:08:12 +0200

    add message service implementation

--- service-client/src/main/java/com/chat/client/service/client/message/MessageService.java
+++ service-client/src/main/java/com/chat/client/service/client/message/MessageService.java
@@ -1,6 +1,24 @@
 package com.chat.client.service.client.message;
 
+import com.chat.server.model.chat.Message;
+
+import java.util.List;
+
 public interface MessageService {
     //@yassmin
     //todo add methods signature for this class
+
+    /**
+     * send message to group
+     *
+     * @param message is sent to users
+     */
+    void sendMessage(Message message);
+
+    /**
+     * this method used to recievethemessage from group
+     *
+     * @return message that from group
+     */
+    List<Message> receiveMessage();
 }
--- service-client/src/main/java/com/chat/client/service/client/message/impl/MessageServiceImpl.java
+++ service-client/src/main/java/com/chat/client/service/client/message/impl/MessageServiceImpl.java
@@ -1,9 +1,24 @@
 package com.chat.client.service.client.message.impl;
 
 import com.chat.client.service.client.message.MessageService;
+import com.chat.server.model.chat.Message;
+
+import java.util.List;
 
 public class MessageServiceImpl implements MessageService {
+
     //@yassmin
     //todo impl this class
     // using the factory object from Repository layer
+    @Override
+    public void sendMessage(Message message) {
+
+    }
+
+    @Override
+    public List<Message> receiveMessage() {
+        return null;
+    }
+
+
 }
