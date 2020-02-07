3eb7f781732fa1ef24f9da27cd141c4b5c770cf5
noura <nouradel975@gmial.com>
2020-02-07 14:37:08 +0200

    inject server-service to client

--- service-client/src/main/java/module-info.java
+++ service-client/src/main/java/module-info.java
@@ -8,5 +8,11 @@ module service.client {
     requires model.server;
     opens com.chat.client.service.client.factory;
     opens com.chat.client.service.client.user;
-
+    opens com.chat.client.service.client.user.impl;
+    opens com.chat.client.service.client.message;
+    opens com.chat.client.service.client.message.impl;
+    opens com.chat.client.service.client.chat;
+    opens com.chat.client.service.client.chat.impl;
+    opens com.chat.client.service.client.notification;
+    opens com.chat.client.service.client.notification.impl;
 }
