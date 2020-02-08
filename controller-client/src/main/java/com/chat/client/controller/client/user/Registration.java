666e785e4752de0a1049706038f6e715a3c98e23
badri <mohamedbadri136@gmail.com>
2020-02-08 09:38:07 +0200

    add login controller

---
+++ controller-client/src/main/java/com/chat/client/controller/client/user/Registration.java
@@ -0,0 +1,21 @@
+package com.chat.client.controller.client.user;
+
+import com.chat.server.model.user.User;
+
+public class Registration {
+
+
+    public Registration() {
+
+    }
+
+    public User login(String phone, String password) {
+
+        return null;
+    }
+
+    public User signup(User user) {
+
+        return user;
+    }
+}
--- controller-client/src/main/java/com/chat/client/controller/client/user/UserController.java
+++ controller-client/src/main/java/com/chat/client/controller/client/user/UserController.java
@@ -5,13 +5,8 @@ import com.chat.client.service.client.user.ClientUserService;
 import com.chat.server.model.user.User;
 
 public class UserController {
-    private ClientUserService userService = ServiceClientFactory.createUserService();
 
     public UserController() {
     }
 
-
-    public User login(String phone, String password) {
-        return userService.login(phone, password);
-    }
 }
