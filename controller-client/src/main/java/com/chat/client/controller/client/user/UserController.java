e8cebcfae1049b805dcc6379f2eb52b01e257d7b
DELL <radwahussien24@gmail.com>
2020-02-08 09:37:43 +0200

    Add loginController interface

--- controller-client/src/main/java/com/chat/client/controller/client/user/UserController.java
+++ controller-client/src/main/java/com/chat/client/controller/client/user/UserController.java
@@ -1,17 +1,10 @@
 package com.chat.client.controller.client.user;
 
-import com.chat.client.service.client.factory.ServiceClientFactory;
-import com.chat.client.service.client.user.ClientUserService;
-import com.chat.server.model.user.User;
 
 public class UserController {
-    private ClientUserService userService = ServiceClientFactory.createUserService();
+
 
     public UserController() {
     }
 
-
-    public User login(String phone, String password) {
-        return userService.login(phone, password);
-    }
 }
